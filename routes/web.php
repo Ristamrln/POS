d124a58e55a12e77919d46fc9e10c49c47e11e83
RistaMarlinaHutagaol <ristamarlina2604@gmail.com>
2024-02-25 21:00:28 +0700

    donerek

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SaleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'home']);
+Route::get('/category/baby-kid',[ProductsController::class,'babyKid']);
+Route::get('/category/beauty-health',[ProductsController::class,'beautyHealth']);
+Route::get('/category/food-beverage',[ProductsController::class,'foodBeverage']);
+Route::get('/category/home-care',[ProductsController::class,'homeCare']);
+
+Route::get('/user/{id}/name/{name}',[UserController::class,'user']);
+
+Route::get('/sale',[SaleController::class,'sale']);
